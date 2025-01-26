fdaba2b455fe4d1976a3c99c439881340b52204f
Kartik Thakur <itzkashyap2009@gmail.com>
2025-01-26 10:55:53 +0530

    second modify

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,45 +1,105 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom';
+import ROUTES from '../navigations/Routes';
 
 function NavBar() {
+  const[user,setUser]=useState({id:null,role:null})
+  const navigate=useNavigate();
+
+  useEffect(()=>{
+    let id=localStorage.getItem("id");
+    let role=localStorage.getItem("role");
+    if(id) setUser({id:id,role:role});
+
+  },[]);
+
+  function renderMenu(){
+    if(user?.role=="admin"){
+      return(
+
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <Link className='nav-link' to={ROUTES.universityAdmin.name}>UniversityManagement</Link>
+        </li>        
+      </ul>
+
+      );
+    }else{
+      return(
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <Link className='nav-link' to={ROUTES.userhome.name}>Home</Link>
+        </li>        
+
+        <li class="nav-item active">
+          <Link className='nav-link' to={ROUTES.about.name}>About</Link>
+        </li> 
+        <li class="nav-item active">
+          <Link className='nav-link' to={ROUTES.contact.name}>Contact</Link>
+        </li>    <li class="nav-item active">
+          <Link className='nav-link' to={ROUTES.support.name}>Support</Link>
+        </li> 
+           
+      </ul>
+
+      );
+    }
+  }
+
+function renderButton() {
+  if (user?.id) {
+    return (
+      <form class="form-inline my-2 my-lg-0">
+        <button
+          class="btn btn-outline-success my-2 my-sm-0"
+          onClick={() => {
+            localStorage.clear();
+            navigate(ROUTES.login.name);
+          }}
+        >
+          Logout
+        </button>
+      </form>
+    );
+  } else {
+    return (
+      <>
+        <form class="form-inline my-2 my-lg-0">
+          <button
+            class="btn btn-outline-info my-2 my-sm-0"
+            onClick={() => {
+              navigate(ROUTES.register.name);
+            }}
+          >
+            Register
+          </button>
+        </form>
+        <form class="form-inline my-2 my-lg-0">
+          <button
+            class="btn btn-outline-primary my-2 my-sm-0"
+            onClick={() => {
+              navigate(ROUTES.login.name);
+            }}
+          >
+            Login
+          </button>
+        </form>
+      </>
+    );
+  }
+}
+
   return (
     <>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          {renderMenu()}
+          {renderButton()}
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+      </nav>
     </>
-    )
+  );
 }
 
 export default NavBar
--- client/src/containers/login/Login.js
+++ client/src/containers/login/Login.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../../components/NavBar'
+import {useNavigate}from'react-router-dom'
+import axios from 'axios';
+import ROUTES from '../../navigations/Routes';
+
 function Login() {
+
+  const[form,setForm]=useState({email:"",password:""});
+  const[formError,setFormError]=useState({email:"",password:""});
+
+  const navigate=useNavigate();
+  
+  const ChangeHandler=(e)=>{
+    setForm({...form,[e.target.name]:e.target.value});
+  }
+
+  function loginCheck(){
+    try {
+      axios.post("http://127.0.0.1:8080/login",form).then((d)=>{
+        localStorage.setItem("id",d.data.id);
+        localStorage.setItem("role",d.data.role);
+
+        if(d.data.role=="admin") navigate(ROUTES.universityAdmin.name)
+          else navigate (ROUTES.userhome.name);
+      }).catch((e)=>{
+        alert("wrong user/pwd");
+        setForm({email:"",password:""});
+      });
+    } catch (error) {
+      alert(error?.message);
+    }
+  }
+  
+  function onLoginSubmit(){
+    let errors=false;
+    let error={email:"",password:""}
+    if(form.email.trim().length==0){
+      errors=true;
+      error={...error,email:"UserName Empty"};
+    }
+    if(form.password.trim().length==0){
+      errors=true;
+      error={...error,password:"UserName Empty"};
+    }
+    if(errors)setFormError(error);
+    else{
+      setFormError(error);
+      loginCheck();
+    }
+  }
+  
+ 
   return (
     <>
-    <NavBar/>
+      <NavBar />
+      <div className='row p-2 m-2'>
+      <div class="card text-center mx-auto">
+        <div class="card-header bg-info text-white">Login</div>
+        <div class="card-body">
+          <div className='form-group row'>
+            <label className='col-4'>UserName</label>
+            <div className='col-8'>
+              <input className='form-control' type='text' name="email" onChange={ChangeHandler}></input>
+            </div>
+          </div>
+          <div className='form-group row'>
+            <label className='col-4'>Password</label>
+            <div className='col-8'>
+              <input className='form-control' type='password' name="password" onChange={ChangeHandler}></input>
+            </div>
+          </div>
+          <button  className='btn btn-success col-4 mx-auto' onClick={()=>{onLoginSubmit()}}>Login</button>
+        </div>        
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Login
